bb4023319d7f7ebc8490be13e63308ed780fc3ec
zxingz <Saibaba_1>
2017-09-02 00:12:42 -0400

    code added

--- script/startNBA.py
+++ script/startNBA.py
@@ -1,13 +1,27 @@
 import os
 import sys
 
+
+
 #initializing environment
 sys.path.append('../../common/script')
 import initEnv
-logger = initEnv.env(__file__).ret()
+logger, bot = initEnv.env(__file__).ret()
 logger.info('starting app: '+os.environ.get('MODULE_NAME'))
 
+class nbaMain():
 
+    def __init__(self, logger, bot):
+        self.logger = logger
+        self.bot = bot
 
-for key in os.environ:
-    print(key, os.environ.get(key))
+    def sendTelegram(self, message):
+        try:
+            self.logger.info('sending telegram: '+message)
+            self.bot.send_message(chat_id=439726750, text=message)
+        except Exception as error:
+            self.logger.info('Telegram failed due to: '+str(error))
+
+if __name__ == '__main__':
+    game = nbaMain(logger, bot)
+    game.sendTelegram('Finished Processing !')
